b192066486098b4908b7f7bf349cf3ab0f2a3b22
Weixin Zhang <zhangweixinxd@gmail.com>
2016-06-19 15:41:14 +0800

    Draft Section 4

--- src/scala/base/Circuit.scala
+++ src/scala/base/Circuit.scala
@@ -1,7 +1,7 @@
 //BEGIN_CIRCUIT_SCALA
 package base
 trait Circuit { def width: Int }
-class Identity(val n: Int) extends Circuit {
+class Id(val n: Int) extends Circuit {
   def width = n
 }
 class Fan(val n: Int) extends Circuit {
