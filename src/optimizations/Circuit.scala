cc68103636cf87c069f5a0d18a2ff0ac414f6e9f
Weixin Zhang <zhangweixinxd@gmail.com>
2016-06-21 14:38:52 +0800

    Shorten S4

--- src/optimizations/Circuit.scala
+++ src/optimizations/Circuit.scala
@@ -1,16 +1,16 @@
 package optimizations
 
 //BEGIN_MERGEIDS_SCALA
-trait Circuit {
+trait Circuit extends width.Circuit {
   def mergeIds: Circuit = this
 }
-trait Id extends Circuit { val n: Int }
+trait Id extends width.Id with Circuit
 object Id {
   def apply(i: Int) = new Id{val n=i}
   def unapply(c: Id) = Some(c.n)
 }
-trait Fan extends Circuit { val n: Int }
-trait Beside extends Circuit {
+trait Fan extends width.Fan with Circuit
+trait Beside extends width.Beside with Circuit {
   val c1, c2: Circuit
   override def mergeIds: Circuit = {
     val (d1,d2) = (c1.mergeIds,c2.mergeIds)
