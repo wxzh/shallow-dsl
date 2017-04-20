0771fa71397dbe830ad76a5b5f4706d10a1abd29
Weixin Zhang <zhangweixinxd@gmail.com>
2017-04-20 13:24:10 +0800

    Add code for response

---
+++ src/scans/ObjectAlgebras.scala
@@ -0,0 +1,127 @@
+package scans
+
+object ObjectAlgebras extends App {
+  // An Object Algebra interface (abstract factory) describing the supported circuit constructors
+  trait CircuitFactory[C] {
+    def identity(x: Int): C
+    def fan(x: Int): C
+    def above(x: C, y: C): C
+    def beside(x: C, y: C): C
+    def stretch(x: C, xs: Int*): C
+  }
+
+  // An Object Algebra (concrete factory) that produces Circuit1 (page 4)
+  class Circuit1Factory extends CircuitFactory[Circuit1] {
+    def identity(x: Int)                  =  new Identity1  {val n=x}
+    def fan(x: Int)                       =  new Fan1       {val n=x}
+    def above(x: Circuit1, y: Circuit1)   =  new Above1     {val c1=x; val c2=y}
+    def beside(x: Circuit1, y: Circuit1)  =  new Beside1    {val c1=x; val c2=y}
+    def stretch(x: Circuit1, xs: Int*)    =  new Stretch1   {val ns=xs.toList; val c=x}
+  }
+
+  // Another Object Algebra that produces Circuit4 (page 8)
+  class Circuit4Factory extends CircuitFactory[Circuit4] {
+    def identity(x: Int)                  =  new Identity4  {val n=x}
+    def fan(x: Int)                       =  new Fan4       {val n=x}
+    def above(x: Circuit4, y: Circuit4)   =  new Above4     {val c1=x; val c2=y}
+    def beside(x: Circuit4, y: Circuit4)  =  new Beside4    {val c1=x; val c2=y}
+    def stretch(x: Circuit4, xs: Int*)    =  new Stretch4   {val ns=xs.toList; val c=x}
+  }
+
+  // An interpretation-independent circuit constructed via an abstract factory (page 5)
+  def c[C](f: CircuitFactory[C]) =
+    f.above(f.beside(f.fan(2),f.fan(2)),
+            f.above(f.stretch(f.fan(2),2,2),
+                    f.beside(f.beside(f.identity(1),f.fan(2)),f.identity(1))))
+
+  def c2[C](f: CircuitFactory[C]) = {
+    import f._
+    above(beside(fan(2),fan(2)),
+            above(stretch(fan(2),2,2),
+                    beside(beside(identity(1),fan(2)),identity(1))))
+  }
+
+  // Supplying concrete factories to allow different interpretations
+  c(new Circuit1Factory).width // 4 
+  c(new Circuit4Factory).tlayout { x => x } // List(List((0,1), (2,3)), List((1,3)), List((1,2)))
+
+  // Circuit constructor extension
+  trait ExtCircuitFactory[C] extends CircuitFactory[C] {
+    def rstretch(x: C, xs: Int*): C
+  }
+  class ExtCircuit4Factory extends Circuit4Factory {
+    def rstretch(x: Circuit4, xs: Int*) = new RStretch {val c=x; val ns=xs.toList}
+  }
+  
+  // Client code for extension
+  def c3[C](f: ExtCircuit4Factory) = {
+    import f._
+    rstretch(c2(f),2,2,2,2)
+  }
+  c3(new ExtCircuit4Factory).tlayout { x => x } // List(List((1,3), (5,7)), List((3,7)), List((3,5)))
+
+  // Code from the paper pasted below
+  trait Circuit1 {
+    def width: Int
+  }
+  trait Identity1 extends Circuit1 {
+    val n: Int
+    def width = n
+  }
+  trait Fan1 extends Circuit1 {
+    val n: Int
+    def width = n
+  }
+  trait Beside1 extends Circuit1 {
+    val c1, c2: Circuit1
+    def width = c1.width + c2.width
+  }
+  trait Above1 extends Circuit1 {
+    val c1, c2: Circuit1
+    def width = c1.width
+  }
+  trait Stretch1 extends Circuit1 {
+    val ns: List[Int]
+    val c: Circuit1
+    def width = ns.sum
+  }
+
+  type Layout = List[List[(Int,Int)]]
+  trait Circuit4 extends Circuit1 {
+    def tlayout(f: Int => Int): Layout
+  }
+  trait Identity4 extends Identity1 with Circuit4 {
+    def tlayout(f: Int => Int) = List()
+  }
+  trait Fan4 extends Fan1 with Circuit4 {
+    def tlayout(f: Int => Int) = List(for (i <- List.range(1,n)) yield (f(0),f(i)))
+  }
+  trait Above4 extends Above1 with Circuit4 {
+    val c1, c2: Circuit4
+    def tlayout(f: Int => Int) = c1.tlayout(f) ++ c2.tlayout(f)
+  }
+  trait Beside4 extends Beside1 with Circuit4 {
+    val c1, c2: Circuit4
+    def tlayout(f: Int => Int) = lzw (c1.tlayout(f), c2.tlayout(f.compose(c1.width + _))) (_ ++ _)
+  }
+  trait Stretch4 extends Stretch1 with Circuit4 {
+    val c: Circuit4
+    def tlayout(f: Int => Int) = {
+      val vs = ns.scanLeft(0)(_ + _).tail
+      c.tlayout(f.compose(vs(_) - 1))
+    }
+  }
+
+  def lzw[A](xs: List[A], ys: List[A])(f: (A, A) => A): List[A] = (xs, ys) match {
+    case (Nil,_)        =>  ys
+    case (_,Nil)        =>  xs
+    case (x::xs,y::ys)  =>  f(x,y) :: lzw (xs,ys) (f)
+  }
+
+  trait RStretch extends Stretch4 {
+    override def tlayout(f: Int => Int) = {
+      val vs = ns.scanLeft(ns.last - 1)(_ + _).init
+      c.tlayout(f.compose(vs(_)))
+    }
+  }
+}
