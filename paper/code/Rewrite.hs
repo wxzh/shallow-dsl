e26dadb57c31b622ebf92ff7e39b484f1f72f032
Weixin Zhang <zhangweixinxd@gmail.com>
2017-12-01 18:21:11 +0800

    Cleanup repo

--- paper/code/Rewrite.hs
+++
@@ -1,6 +0,0 @@
---BEGIN_CIRCUIT3
-newtype Circuit = Circuit {width :: Int}
-id n         = Circuit {width = n}
-fan n        = Circuit {width = n}
-beside c1 c2 = Circuit {width = width c1 + width c2}
---END_CIRCUIT3
